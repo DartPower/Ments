baea4a6885d78b981d057bef23846b2375e37e79
DartPower <>
2020-12-22 20:31:57 +0300

    Fix

--- src/main/java/io/dpteam/Ments/Main.java
+++ src/main/java/io/dpteam/Ments/Main.java
@@ -26,11 +26,11 @@ public class Main extends JavaPlugin implements Listener {
 		this.getConfig().options().copyDefaults(true);
 		this.saveConfig();
 		this.getServer().getPluginManager().registerEvents(this, this);
-		Bukkit.getServer().getLogger().info("[DPT.MC] Ments Enabled");
+		this.getServer().getLogger().info("[DPT.MC] Ments Enabled");
 	}
 
 	public void onDisable() {
-		Bukkit.getServer().getLogger().info("[DPT.MC] Ments Disabled");
+		this.getServer().getLogger().info("[DPT.MC] Ments Disabled");
 	}
 
 	@EventHandler
